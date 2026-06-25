c7bc79e03fc0186d6123490c58301a2f3fed7cb4
MA-Alumno <MA-Alumno@duoc.acad>
2026-06-24 21:22:35 -0400

    Evidencias completas EP3 - Maximiliano Cifuentes

--- ProyectoMicroservicios/Inventario/src/main/java/com/inventario/clduoc/Inventario/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.inventario.clduoc.Inventario.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
